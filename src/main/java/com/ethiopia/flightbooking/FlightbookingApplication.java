8f133865b3bfb966d2d8f142bc6b30e771a989b8
zelalemzergaw <zola145103@gmail.com>
2019-10-14 15:01:06 -0500

    "initial commit"

---
+++ src/main/java/com/ethiopia/flightbooking/FlightbookingApplication.java
@@ -0,0 +1,13 @@
+package com.ethiopia.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightbookingApplication.class, args);
+    }
+
+}
