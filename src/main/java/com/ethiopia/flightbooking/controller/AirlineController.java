c15cb964f69db043c2243fb091589684beefdf34
zelalemzergaw <zola145103@gmail.com>
2019-10-15 08:49:15 -0500

    added anotations in service class and working on Airline controller

--- src/main/java/com/ethiopia/flightbooking/controller/AirlineController.java
+++ src/main/java/com/ethiopia/flightbooking/controller/AirlineController.java
@@ -3,7 +3,10 @@ package com.ethiopia.flightbooking.controller;
 import com.ethiopia.flightbooking.service.AirlineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/airline")
@@ -11,5 +14,11 @@ public class AirlineController {
 
     @Autowired
     public AirlineService airlineService;
+
+    @GetMapping(value = "index")
+    public String getAirLines(Model model){
+    model.addAttribute("airlines",airlineService.findAll());
+    return "airline/index";
+    }
 
 }
